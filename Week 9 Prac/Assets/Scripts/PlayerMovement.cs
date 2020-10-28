a3007166960be0abcce105c80c083c33a1cb69b7
salangsangria <53996461+salangsangria@users.noreply.github.com>
2020-10-29 10:05:43 +1100

    Player and PlayerMovement

---
+++ Week 9 Prac/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(speed * Time.deltaTime * (-1), 0, 0);
+        }
+        //right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime * (1), 0, 0);
+        }
+        //up
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, speed * Time.deltaTime * (1), 0);
+        }
+        //down
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, speed * Time.deltaTime * (-1), 0);
+        }
+    }
+}
