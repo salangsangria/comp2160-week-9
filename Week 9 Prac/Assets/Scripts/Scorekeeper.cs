70de2fadd8a1d6ea2efdf569509dbb55a5c854f3
salangsangria <53996461+salangsangria@users.noreply.github.com>
2020-10-29 09:18:22 +1100

    Scorekeeper

---
+++ Week 9 Prac/Assets/Scripts/Scorekeeper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorekeeper : MonoBehaviour
+{
+    public int pickupPoints;
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+
+    public void AddScore()
+    {
+        score += pickupPoints;
+    }
+}
